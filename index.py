91350bdcd3502ea1c902eca23dbf23eba941dd6f
EricWithC04 <negativegame117@gmail.com>
2024-01-22 20:58:57 -0300

    background in movement

--- index.py
+++ index.py
@@ -3,7 +3,19 @@ from pygame.locals import *
 
 pygame.init()
 
-SCREEN = pygame.display.set_mode((800, 480))
+screen_d = {
+    "w": 800,
+    "h": 480
+}
+
+FPS = 30
+clock = pygame.time.Clock()
+
+SCREEN = pygame.display.set_mode((
+    screen_d["w"], 
+    screen_d["h"]
+))
+
 pygame.display.set_caption("Mi primer juego!")
 
 colors = {
@@ -14,8 +26,12 @@ colors = {
     "green": (0, 255, 0),
 }
 
-bg = pygame.image.load("static/assets/background.jpg")
-SCREEN.blit(bg, (0, 0))
+bg = pygame.image.load("static/assets/background.jpg").convert()
+bg_position = {
+    "x": 0,
+    "y": 0
+}
+
 pygame.draw.rect(SCREEN, colors["red"], (50, 40, 80, 48))
 pygame.draw.line(SCREEN, colors["green"], (50, 88), (200, 88), 10)
 pygame.draw.circle(SCREEN, colors["black"], (300, 88), 20, 15)
@@ -33,4 +49,12 @@ while True:
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    x_relative = bg_position["x"] % SCREEN.get_rect().width
+    SCREEN.blit(bg, (x_relative - SCREEN.get_rect().width, bg_position["y"]))
+
+    if x_relative < screen_d["w"]:
+        SCREEN.blit(bg, (x_relative, bg_position["y"]))
+
+    bg_position["x"] = bg_position["x"] - 5
     pygame.display.update()
+    clock.tick(FPS)
